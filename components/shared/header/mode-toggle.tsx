9271903ff0ccbb0f367b6dddb9947388146971b8
Jaspal Singh <84974169+JaspalSingh1998@users.noreply.github.com>
2024-12-31 09:03:46 -0500

    Added dark mode light mode and system mode support

---
+++ components/shared/header/mode-toggle.tsx
@@ -0,0 +1,50 @@
+"use client";
+import { useState, useEffect } from "react";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { MoonIcon, SunIcon, SunMoonIcon } from "lucide-react";
+import { useTheme } from "next-themes";
+import { DropdownMenuSeparator } from "@radix-ui/react-dropdown-menu";
+const ModeToggle = () => {
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme } = useTheme();
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild className="focus-visible:ring-0 focus-visible:ring-offset-0">
+        <Button variant="ghost" size="sm">
+          {theme === "system" ? (
+            <SunMoonIcon />
+          ) : theme === "dark" ? (
+            <MoonIcon />
+          ) : (
+            <SunIcon />
+          )}
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuLabel>Apperance</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuCheckboxItem checked={theme === "system"} onClick={() => setTheme("system")}>System</DropdownMenuCheckboxItem>
+        <DropdownMenuCheckboxItem checked={theme === "dark"} onClick={() => setTheme("dark")}>Dark</DropdownMenuCheckboxItem>
+        <DropdownMenuCheckboxItem checked={theme === "light"} onClick={() => setTheme("light")}>Light</DropdownMenuCheckboxItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+export default ModeToggle;
