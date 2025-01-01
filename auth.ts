c801b4f5a340919a7f75f7a312c7d18450ee8882
Jaspal Singh <84974169+JaspalSingh1998@users.noreply.github.com>
2025-01-01 14:26:21 -0500

    integrated authjs

---
+++ auth.ts
@@ -0,0 +1,63 @@
+import NextAuth, { NextAuthConfig } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "@/db/prisma";
+import { compareSync } from "bcrypt-ts-edge";
+
+export const config = {
+  pages: {
+    signIn: "/sign-in",
+    error: "/sign-in",
+  },
+  session: {
+    strategy: "jwt",
+    maxAge: 30 * 24 * 60 * 60, // 30 days
+  },
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    CredentialsProvider({
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        const user = await prisma.user.findUnique({
+          where: {
+            email: credentials?.email as string,
+          },
+        });
+        if (user && user.password) {
+          const isMatch = await compareSync(
+            credentials?.password as string,
+            user.password
+          );
+          if (isMatch) {
+            return {
+              id: user.id,
+              name: user.name,
+              email: user.email,
+              role: user.role,
+            };
+          }
+        }
+
+        // If user does not exist or password is incorrect
+        return null;
+      },
+    }),
+  ],
+  callbacks: {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    async session({ session, user, trigger, token }: any) {
+      session.user.id = token.sub;
+
+      if (trigger === "update") {
+        session.user.name = user.name;
+      }
+
+      return session;
+    },
+  },
+} satisfies NextAuthConfig;
+
+export const { handlers, auth, signIn, signOut } = NextAuth(config);
