5116d5276dedaffae8a92d81dc4091027c09e648
Jaspal Singh <84974169+JaspalSingh1998@users.noreply.github.com>
2025-01-02 13:20:46 -0500

    Added signup form ui and actions and integrated

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,9 +1,54 @@
-
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { APP_NAME } from "@/lib/constants";
+import Image from "next/image";
+import Link from "next/link";
+import { redirect } from "next/navigation";
+import { auth } from "@/auth";
+import SignUpForm from "./sign-up-form";
 export const metadata = {
-    title: "Sign up",
-}
-const SignUpPage = () => {
-    return ( <>Heyyy</> );
-}
- 
+  title: "Sign Up",
+};
+const SignUpPage = async (props: {
+  searchParams: Promise<{ callbackUrl: string }>;
+}) => {
+  const { callbackUrl } = await props.searchParams;
+
+  const session = await auth();
+
+  if (session) {
+    return redirect(callbackUrl || "/");
+  }
+
+  return (
+    <div className="w-full max-w-md mx-auto">
+      <Card>
+        <CardHeader className="space-y-4">
+          <Link href="/" className="flex-center">
+            <Image
+              src="/images/logo.svg"
+              alt={`${APP_NAME} logo`}
+              width={48}
+              height={48}
+              priority={true}
+            />
+          </Link>
+          <CardTitle className="text-center">Sign Up</CardTitle>
+          <CardDescription className="text-center">
+            Enter your details below to create your account
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="space-y-4">
+          <SignUpForm />
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
 export default SignUpPage;
