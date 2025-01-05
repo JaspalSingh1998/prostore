52260145ba0bdc9386b1c186ccb4855a73b7f65e
Jaspal Singh <84974169+JaspalSingh1998@users.noreply.github.com>
2025-01-05 15:04:36 -0500

    Added Cart table

--- lib/utils.ts
+++ lib/utils.ts
@@ -18,28 +18,49 @@ export function formatNumberWithDecimals(value: number): string {
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function formatError(error: any) {
   if (error.name === "ZodError") {
-    const fieldErrors = Object.keys(error.errors).map((field) => error.errors[field].message);
+    const fieldErrors = Object.keys(error.errors).map(
+      (field) => error.errors[field].message
+    );
 
     return fieldErrors.join(". ");
   } else if (
     error.name === "PrismaClientKnownRequestError" &&
     error.code === "P2002"
   ) {
-    const field = error.meta?.target ? error.meta.target[0] : 'Field';
+    const field = error.meta?.target ? error.meta.target[0] : "Field";
 
     return `${field.charAt(0).toUpperCase() + field.slice(1)} already exists`;
   } else {
-    return typeof error.message === "string" ? error.message : JSON.stringify(error.message);
+    return typeof error.message === "string"
+      ? error.message
+      : JSON.stringify(error.message);
   }
 }
 
 // Round number to 2 decimal places
 export function round2(value: number | string) {
-  if(typeof value === "number") {
+  if (typeof value === "number") {
     return Math.round((value + Number.EPSILON) * 100) / 100;
-  } else if(typeof value === 'string') {
-    return Math.round((Number(value) + Number.EPSILON) * 100 ) / 100
+  } else if (typeof value === "string") {
+    return Math.round((Number(value) + Number.EPSILON) * 100) / 100;
   } else {
-    throw new Error('Value must be a number or string');
+    throw new Error("Value must be a number or string");
+  }
+}
+
+const CURRENCY_FORMATTER = new Intl.NumberFormat('en-US', {
+  currency: 'USD',
+  style: 'currency',
+  minimumFractionDigits: 2,
+});
+
+// Format currency using the formatter above
+export function formatCurrency(amount: number | string | null) {
+  if (typeof amount === 'number') {
+    return CURRENCY_FORMATTER.format(amount);
+  } else if (typeof amount === 'string') {
+    return CURRENCY_FORMATTER.format(Number(amount));
+  } else {
+    return 'NaN';
   }
 }
