813ff188e6dacf8080bab292eca7c55938d6b833
Jaspal Singh <84974169+JaspalSingh1998@users.noreply.github.com>
2025-01-06 12:58:01 -0500

    added payment method form

--- app/(root)/payment-method/page.tsx
+++ app/(root)/payment-method/page.tsx
@@ -1,6 +1,25 @@
-const PaymentMethodPage = () => {
+import { auth } from "@/auth";
+import { getUserById } from "@/lib/actions/user.actions";
+import { Metadata } from "next";
+import PaymentMethodForm from "./payment-method-form";
+import CheckoutSteps from "@/components/shared/checkout-steps";
+
+export const metadata: Metadata = {
+    title: 'Payment Method',
+} 
+const PaymentMethodPage = async () => {
+    const session = await auth();
+    const userId = session?.user?.id;
+
+    if(!userId) throw new Error('No user ID');
+
+    const user = await getUserById(userId);
+
     return ( 
-        <div>PaymentMethodPage</div>
+        <>
+            <CheckoutSteps current={2} />
+            <PaymentMethodForm preferredPaymentMethod={user.paymentMethod}/>
+        </>
      );
 }
  
