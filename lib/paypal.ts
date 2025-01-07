939fa8843fe59c5811d72a207dcb3464c5c89401
Jaspal Singh <84974169+JaspalSingh1998@users.noreply.github.com>
2025-01-07 12:28:41 -0500

    added create order and capture payment functions

--- lib/paypal.ts
+++ lib/paypal.ts
@@ -1,10 +1,52 @@
 const base = process.env.PAYPAL_API_URL || "https://api.sandbox.paypal.com";
 
-export const paypal = {};
+export const paypal = {
+  createOrder: async function createOrder(price: string) {
+    const accessToken = await generateAccessToken();
+    const url = `${base}/v2/checkout/orders`;
+
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${accessToken}`,
+      },
+      body: JSON.stringify({
+        intent: "CAPTURE",
+        purchase_units: [
+          {
+            amount: {
+              currency_code: "USD",
+              value: price,
+            },
+          },
+        ],
+      }),
+    });
+
+    return handleResponse(response);
+  },
+  capturePayment: async function capturePayment(orderId: string) {
+    const accessToken = await generateAccessToken();
+    const url = `${base}/v2/checkout/orders/${orderId}/capture`;
+
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+
+    return handleResponse(response);
+  }
+};
 
 async function generateAccessToken() {
   const { PAYPAL_CLIENT_ID, PAYPAL_APP_SECRET } = process.env;
-  const auth = Buffer.from(`${PAYPAL_CLIENT_ID}:${PAYPAL_APP_SECRET}`).toString("base64");
+  const auth = Buffer.from(`${PAYPAL_CLIENT_ID}:${PAYPAL_APP_SECRET}`).toString(
+    "base64"
+  );
 
   const response = await fetch(`${base}/v1/oauth2/token`, {
     method: "POST",
@@ -15,13 +57,17 @@ async function generateAccessToken() {
     body: "grant_type=client_credentials",
   });
 
+  const jsonData = await handleResponse(response);
+  return jsonData.access_token;
+}
+
+async function handleResponse(response: Response) {
   if (response.ok) {
-    const jsonData = await response.json();
-    return jsonData.access_token;
-  }else {
-      const errorMessage = await response.text();
-      throw new Error(errorMessage);
+    return response.json();
+  } else {
+    const errorMessage = await response.text();
+    throw new Error(errorMessage);
   }
 }
 
-export {generateAccessToken};
+export { generateAccessToken };
