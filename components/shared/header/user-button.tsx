df6025b181ba0e9654c09de5f6ea5c8784d837fd
Jaspal Singh <84974169+JaspalSingh1998@users.noreply.github.com>
2025-01-11 16:27:19 -0500

    Added admin route and types for next auth and updated user button

--- components/shared/header/user-button.tsx
+++ components/shared/header/user-button.tsx
@@ -31,7 +31,10 @@ const UserButton = async () => {
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
           <div className="flex items-center">
-            <Button variant="ghost" className="relative size-8 rounded-full ml-2 flex items-center justify-center bg-gray-200">
+            <Button
+              variant="ghost"
+              className="relative size-8 rounded-full ml-2 flex items-center justify-center bg-gray-200"
+            >
               {firstInitial}
             </Button>
           </div>
@@ -39,16 +42,30 @@ const UserButton = async () => {
         <DropdownMenuContent className="w-56" align="end" forceMount>
           <DropdownMenuLabel className="font-normal">
             <div className="flex flex-col space-y-1">
-                <div className="text-sm font-medium leading-none">{session.user?.name}</div>
-                <div className="text-sm text-muted-foreground leading-none">{session.user?.email}</div>
+              <div className="text-sm font-medium leading-none">
+                {session.user?.name}
+              </div>
+              <div className="text-sm text-muted-foreground leading-none">
+                {session.user?.email}
+              </div>
             </div>
           </DropdownMenuLabel>
           <DropdownMenuItem>
-            <Link href='/user/orders'>Order History</Link>
+            <Link href="/user/orders">Order History</Link>
           </DropdownMenuItem>
+          {session.user?.role === "admin" && (
+            <DropdownMenuItem>
+              <Link href="/admin/overview">Admin</Link>
+            </DropdownMenuItem>
+          )}
           <DropdownMenuItem className="p-0 mb-1">
             <form action={signOutUser} className="w-full">
-                <Button className="w-full py-4 px-2 h-4 justify-start" variant='ghost'>Sign out</Button>
+              <Button
+                className="w-full py-4 px-2 h-4 justify-start"
+                variant="ghost"
+              >
+                Sign out
+              </Button>
             </form>
           </DropdownMenuItem>
         </DropdownMenuContent>
